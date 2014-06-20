b7952127622c867b4143f313d9a5c320b0b995f8
Tatsunori TAMAKI <qtamaki@gmail.com>
2014-06-20 13:05:23 +0900

    event on show.

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,73 +1,74 @@
 (function($){
- 
-    $.fn.extend({ 
-         
-        leanModal: function(options) {
- 
-            var defaults = {
-                top: 100,
-                overlay: 0.5,
-                closeButton: null
-            }
-            
-            var overlay = $("<div id='lean_overlay'></div>");
-            
-            $("body").append(overlay);
-                 
-            options =  $.extend(defaults, options);
- 
-            return this.each(function() {
-            
-                var o = options;
-               
-                $(this).click(function(e) {
-              
-              	var modal_id = $(this).attr("href");
 
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id);                    
-                });
-                
-                $(o.closeButton).click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
+	$.fn.extend({
 
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+		leanModal: function(options) {
 
-        		$('#lean_overlay').fadeTo(200,o.overlay);
+			var defaults = {
+				top: 100,
+				overlay: 0.5,
+				closeButton: null,
+				afterShow: null
+			}
 
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
+			var overlay = $("<div id='lean_overlay'></div>");
 
-        		$(modal_id).fadeTo(200,1);
+			$("body").append(overlay);
 
-                e.preventDefault();
-                		
-              	});
-             
-            });
+			options =  $.extend(defaults, options);
+
+			return this.each(function() {
+
+				var o = options;
+
+				$(this).click(function(e) {
+
+					var modal_id = $(this).attr("href");
+
+					$("#lean_overlay").click(function() {
+						close_modal(modal_id);
+					});
+
+					$(o.closeButton).click(function() {
+						close_modal(modal_id);
+					});
+
+					var modal_height = $(modal_id).outerHeight();
+					var modal_width = $(modal_id).outerWidth();
+
+					$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+
+					$('#lean_overlay').fadeTo(200,o.overlay);
+
+					$(modal_id).css({
+						'display' : 'block',
+						'position' : 'fixed',
+						'opacity' : 0,
+						'z-index': 11000,
+						'left' : 50 + '%',
+						'margin-left' : -(modal_width/2) + "px",
+						'top' : o.top + "px"
+					});
+
+					$(modal_id).fadeTo(200,1);
+
+					if(o.afterShow){ o.afterShow(e); }
+
+					e.preventDefault();
+
+				});
+
+			});
 
 			function close_modal(modal_id){
 
-        		$("#lean_overlay").fadeOut(200);
+				$("#lean_overlay").fadeOut(200);
 
-        		$(modal_id).css({ 'display' : 'none' });
-			
+				$(modal_id).css({ 'display' : 'none' });
+
 			}
-    
-        }
-    });
-     
+
+		}
+	});
+
 })(jQuery);
